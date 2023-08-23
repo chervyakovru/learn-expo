ac634de41ec8f6bd63827e2f84ba1b834e694ba5
Daniil Chervyakov <da-chervyakov@yandex-team.ru>
2023-08-23 16:28:10 +0300

    add saving image to web

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { LegacyRef, Ref, useRef, useState } from 'react';
+import { useRef, useState } from 'react';
+import { StyleSheet, View, Image, ViewComponent, Platform } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image, ViewComponent } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import * as MediaLibrary from 'expo-media-library';
 import { captureRef } from 'react-native-view-shot';
+import domtoimage from 'dom-to-image';
 
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
@@ -47,18 +48,34 @@ export default function App() {
   };
 
   const onSaveImageAsync = async () => {
-    try {
-      const localUri = await captureRef(imageRef, {
-        height: 440,
-        quality: 1,
-      });
-
-      await MediaLibrary.saveToLibraryAsync(localUri);
-      if (localUri) {
-        alert("Saved!");
+    if (Platform.OS !== 'web') {
+      try {
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert('Saved!');
+        }
+      } catch (e) {
+        console.log(e);
       }
-    } catch (e) {
-      console.log(e);
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current! as unknown as Node, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement('a');
+        link.download = 'sticker-smash.jpeg';
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
     }
   };
 
